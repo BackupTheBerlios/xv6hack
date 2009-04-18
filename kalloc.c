bbe805949a9acf326a08f293676d60f17da5f055
Dave Young <hidave.darkstar@gmail.com>
2009-04-18 20:14:55 +0800

    Add more functions for slab implementation

--- kalloc.c
+++ kalloc.c
@@ -134,18 +134,21 @@ kalloc(int n)
 
 
 #define getpage()	kalloc(PAGE)
-#define freepage(p)	kfree(p)
+#define putpage(p)	kfree(p, PAGE)
 #define OBJ_MIN		8
 #define OBJ_MAX		4096
 #define SLAB_MAGIC_UNINIT 0x12345678
 #define SLAB_MAGIC_INIT 0xabbaabba
+#define SLAB_FREE_MAX	8
 
-/* one cache - one slab - one page */
+/* cache - slabs (one slab - one page) */
 
 struct cache {
 	struct cache *next;
-	void *slist; /* full ... partial slab list head */
-	void *spart; /* the least free partial slab */
+	struct cache *prev;
+	struct slab *sfull; /* the full slab */
+	struct slab *spart; /* the partial slab */
+	struct slab *sfree; /* the free slab */
 	int objsize; /* object size */
 	void (*ctor)(void *); /* creator */ 
 	void (*dtor)(void *); /* destroctor */
@@ -154,10 +157,13 @@ struct cache {
 
 struct slab {
 	struct cache *cache;
+	struct slab *next;
+	struct slab *prev;
 	void *obj; /* first free obj */
 	struct spinlock lock;
 	unsigned char objmap[64]; /* obj usage bitmap one bit for one obj*/
-	unsigned int magic; /* magic number to indicate initilize state */
+	unsigned int which; /* belong to which list full/free/partial */
+	unsigned int magic;  /* magic number to indicate initilize state */
 };
 
 static inline void slab_setbit(struct slab *slabp, int i)
@@ -167,7 +173,7 @@ static inline void slab_setbit(struct slab *slabp, int i)
 	j = i/8;
 	k = i%8;
 
-	slabp->objmap[j] |= 1 << (8-k);
+	slabp->objmap[j] |= 1 << k;
 }
 
 static inline void slab_clrbit(struct slab *slabp, int i)
@@ -177,18 +183,46 @@ static inline void slab_clrbit(struct slab *slabp, int i)
 	j = i/8;
 	k = i%8;
 
-	/* ... */
+	slabp->objmap[j] &= ~(1 << k);
 }
 
 static inline int slab_first_free_bit(struct slab *slabp)
 {
-	return 0;
+	int i, j, k = -1;
+
+	for (i = 0; i<64 ;i++)
+		for (j = 0; j < 8; j++)
+			if (!(slabp->objmap[i] & (1 << j))) {
+				k = i * 8 + j;
+				break;
+			}
+	return k;
 }
 
 static inline int slab_free_bit_count(struct slab *slabp)
 {
-	return 0;
+	int i, j, k = 0;
+
+	for (i = 0; i<64 ;i++)
+		for (j = 0; j < 8; j++)
+			if (slabp->objmap[i] & (1 << j))
+				k++;
+	return k;
 }
+
+int slab_obj_all_free(struct slab *slabp)
+{
+	int i, j, k = 1;
+
+	for (i = 0; i<64 ;i++)
+		for (j = 0; j < 8; j++)
+			if (slabp->objmap[i] & (1 << j)) {
+				k = 0;
+				break;
+			}
+	return k;
+}
+
 
 struct cache *cache_head;
 struct cache *cache_tail;
@@ -199,8 +233,10 @@ struct cache cache_cache;
 void kmem_cache_init(void)
 {
 	cache_cache.next = NULL;
-	cache_cache.slist = NULL;
+	cache_cache.prev= NULL;
+	cache_cache.sfull= NULL;
 	cache_cache.spart = NULL;
+	cache_cache.sfree = NULL;
 	cache_cache.objsize = sizeof(struct cache);
 	cache_cache.ctor = NULL;
 	cache_cache.dtor = NULL;
@@ -236,7 +272,7 @@ static int kmem_cache_grow(struct cache *cache)
 	memset(slabp->objmap, 0, 64);
 
 	if (cache->ctor)
-		while (mem < slabp) {
+		while (mem < (void *)slabp) {
 			cache->ctor(mem);
 			mem += cache->objsize;
 		}
@@ -244,53 +280,159 @@ static int kmem_cache_grow(struct cache *cache)
 	slabp->magic = SLAB_MAGIC_INIT;
 
 link:
-	if(!cache->slist)
-		cache->slist = slabp;
-	cache->spart = slabp;
+	slabp->next = cache->sfree;
+	cache->sfree->prev = slabp;
 
 	return 0;
 }
 
+static inline int freelist_cnt(struct slab *s)
+{
+	int i = 0;
+
+	while (s) {
+		i++;
+		s = s->next;
+	}
+
+	return i;
+}
+
+/* free the slab */
+static int slab_free(struct slab *slabp)
+{
+	int f;
+
+	f = freelist_cnt(slabp);
+
+	slabp->prev->next = slabp->next;
+	slabp->next->prev = slabp->prev;
+	slabp->prev = NULL;
+	slabp->next = NULL;
+
+	if (f > SLAB_FREE_MAX) {
+		/* return slab to page pool */
+		putpage((void *)(slabp + sizeof(struct slab) - PAGE));
+	} else { 
+		slabp->next = slabp->cache->sfree;
+		slabp->cache->sfree->prev = slabp;
+	}
+
+	return 0;
+}
+
+void update_obj(struct slab *slabp)
+{
+	int i, size;
+
+	i = slab_first_free_bit(slabp);
+	size = slabp->cache->objsize;
+
+	slabp->obj = slabp + sizeof(struct slab) - PAGE + i * size;
+}
+
+void part_to_full(struct slab *slabp)
+{
+
+}
+
+void full_to_part(struct slab *slabp)
+{
+
+}
+
 static inline void *getobj(struct slab *slabp)
 {
-	int size, max, i;
+	int i;
 	void *obj = slabp->obj;
 
-	size = slabp->cache->objsize;
-	if ((PAGE - ((uint)obj & (PAGE - 1)) - sizeof(struct slab)) < size) {
+	if (!slab_free_bit_count(slabp)) {
 		cprintf("No space in this slab\n");
 		return NULL;
 	}
 
-	max = PAGE / size;
-	i = ((uint)obj & (PAGE-1))/size;
+	i = slab_first_free_bit(slabp);
 
 	slab_setbit(slabp, i);
-	slabp->obj += size;
+	if (slab_free_bit_count(slabp))
+		update_obj(slabp);
+	else {
+		slabp->obj = NULL;
+		part_to_full(slabp);
+	}
 
 	return obj;
+}
+
+/*
+ * 1. set bitmap
+ * 2. reinit (ctor)
+ * 3. if whole slab free, free the slab, update link lists
+ */
+static inline void putobj(struct slab *slabp, void *obj)
+{
+	int size, i;
+
+	size = slabp->cache->objsize;
+	i = ((uint)obj & (PAGE - 1))/size;
+
+	if (!slab_free_bit_count(slabp))
+		full_to_part(slabp);
+	slab_clrbit(slabp, i);
+	slabp->cache->ctor(obj);
+	update_obj(slabp);
+
+	if (slab_obj_all_free(slabp))
+		slab_free(slabp);
+}
+
+struct slab *find_part(struct cache *cache)
+{
+	struct slab *p = NULL;
+
+	for(p = cache->spart; p; p = p->next) {
+		if (slab_free_bit_count(p))
+			break;
+	}
+
+	return p;
+}
+
+struct slab *free_to_part(struct cache *cache)
+{
+	struct slab *slabp;
+
+	slabp = cache->sfree;
+
+	cache->sfree = slabp->next;
+	cache->sfree->prev = NULL;
+
+	slabp->next = cache->spart;
+	cache->spart->prev = slabp;
+
+	return slabp;
 }
 
 void *kmem_cache_alloc(struct cache *cache)
 {
 	struct slab *slabp;
-	void *mem = NULL;
 
 	acquire(&cache->lock);
 
-	if (!cache->spart) {
+	slabp = find_part(cache);
+	if (slabp)
+		goto ge;
+
+	if (!cache->sfree) {
 		kmem_cache_grow(cache);
-		/* there's no free/partial slab, need create one */
+		/* there's no free slab, need create one */
 	} 
 
-	slabp = cache->spart;
-
-	/* alloc one object from the slab */
-	mem = getobj(slabp);
-
+	slabp = free_to_part(cache);
+ge:
 	release(&cache->lock);
-
-	return mem;
+	/* alloc one object from the slab */
+	return getobj(slabp);
 }
 
 struct cache *kmem_cache_create(int size, void (*ctor)(void*),
@@ -328,27 +470,6 @@ out:
 	return NULL;
 }
 
-static inline void putobj(struct slab *slabp, void *obj)
-{
-	/*
-	 * 1. set bitmap
-	 * 2. reinit (ctor)
-	 * 3. if whole slab free, free the slab, update link lists
-	 */
-}
-
-static int get_freecount(struct slab *slabp)
-{
-	/* calculate then return the bitmap free obj count */
-	return 0;
-}
-
-static int slab_free(struct slab *slabp)
-{
-	/* free the slab to page cache */
-	return 0;
-}
-
 int kmem_cache_free(void *obj)
 {
 	struct slab *slabp;
@@ -358,7 +479,7 @@ int kmem_cache_free(void *obj)
 	putobj(slabp, obj);
 	
 	/* if all 0 release the slab*/
-	if(!get_freecount(slabp))
+	if(!slab_free_bit_count(slabp))
 		slab_free(slabp);
 
 	return 0;
